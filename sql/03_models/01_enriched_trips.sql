2024507fa51718597688b6c7d8b079ad9511a7ee
Sabir Shakhawat <sabirshakhawat101@gmail.com>
2026-09-07 18:10:13 -0400

    enriched the trips further by adding a real pickup location/dropoff metric

---
+++ sql/03_models/01_enriched_trips.sql
@@ -0,0 +1,25 @@
+CREATE OR REPLACE VIEW enriched_yellow_trips AS (
+    SELECT
+    trips.*,
+    pickup.Zone AS pickup_zone,
+    pickup.Borough AS pickup_borough,
+    dropoff.Zone AS dropoff_zone,
+    dropoff.Borough AS dropoff_borough, 
+    CAST(trips.tpep_pickup_datetime AS DATE) AS pickup_date
+FROM stg_yellow_trips AS trips
+
+LEFT JOIN read_csv('data/reference/taxi_zone_lookup.csv') AS pickup
+    ON trips.PULocationID = pickup.LocationID
+
+LEFT JOIN read_csv('data/reference/taxi_zone_lookup.csv') AS dropoff
+    ON trips.DOLocationID = dropoff.LocationID
+
+WHERE trips.is_valid_completed_trip = TRUE
+)
+
+SELECT *
+from enriched_yellow_trips
+limit 100
+
+
+    
